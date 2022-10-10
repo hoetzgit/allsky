0a1d074832dfca36694fedaac23314a18d55bdae
hoetzgit <41108116+hoetzgit@users.noreply.github.com>
2022-10-10 08:21:02 +0200

    myconfig

---
+++ config/uservariables.sh
@@ -0,0 +1,13 @@
+# Holds all the images without overlay on a per-day basis.
+ALLSKY_IMAGES_CLEAN="${ALLSKY_IMAGES}/clean"
+
+# Holds all the images wit compass overlay on a per-day basis.
+ALLSKY_IMAGES_COMPASS="${ALLSKY_IMAGES}/compass"
+
+# Location of optional allsky-website package.
+ALLSKY_WEBSITE="/var/www/html/allsky-website"
+
+# Praefix saveImage.sh
+# Example: saveImage.sh with task-spooler
+# SAVEIMAGE_PRAEFIX="ts -n "
+export SAVEIMAGE_PRAEFIX=""
