ee3bb54901bf6667bd57ea0e8864c7692232da2f
Eric Claeys <83164203+EricClaeys@users.noreply.github.com>
2026-05-10 20:07:58 -0500

    Update capture_RPi.cpp: Let closeUp() display message

--- src/capture_RPi.cpp
+++ src/capture_RPi.cpp
@@ -974,7 +974,6 @@ myModeMeanSetting.modeMean = CG.myModeMeanSetting.modeMean;
 
 				// See if we should exit.
 				if (CG.maxImages > 0 && numExposures >= CG.maxImages) {
-					Log(1, "Stopping after %d image%s.\n", CG.maxImages, CG.maxImages == 1 ? "" : "s");
 					closeUp(EXIT_STOP);
 				}
 
